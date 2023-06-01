ba489fa1a191ea1e716fb48e9f4d045c82026bb1
KatisoTheGreat <katisoledwaba@gmail.com>
2023-06-01 17:13:37 +0200

    updated styling

--- src/components/Interests.js
+++ src/components/Interests.js
@@ -4,7 +4,7 @@ function Interests() {
     return(
         <div className="interest">
             <h4>Interests</h4>
-            <p>Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
+            <p>I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
         </div>
     )
 }
